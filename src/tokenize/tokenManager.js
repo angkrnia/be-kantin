3b89ce998e418755aee55ae8ccd5d94c447e9906
angkrnia <anggakurnia712@gmai.com>
2023-06-16 16:19:53 +0700

    add tokenManager

---
+++ src/tokenize/tokenManager.js
@@ -0,0 +1,19 @@
+const Jwt = require("@hapi/jwt");
+const InvariantError = require("../error/InvarianError");
+
+const TokenManager = {
+  generateAccessToken: async (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
+  generateRefreshToken: async (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+  verifyRefreshToken: async (refreshToken) => {
+    try {
+      const artifacts = Jwt.token.decode(refreshToken);
+      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
+      const { payload } = artifacts.decoded;
+      return payload;
+    } catch (error) {
+      throw new InvariantError("Refresh token tidak valid");
+    }
+  },
+};
+
+module.exports = TokenManager;
